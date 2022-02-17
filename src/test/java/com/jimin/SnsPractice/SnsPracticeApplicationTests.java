9972216828a35eba9df29391a4ced8d159789522
begrey <cini1111@naver.com>
2022-02-17 13:10:59 +0900

    Initial commit

---
+++ src/test/java/com/jimin/SnsPractice/SnsPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jimin.SnsPractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnsPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
